c18d855ad26b636e03236c0eb67edcf945a2c0bb
khangkaka066 <knguyennht@gmail.com>
2026-09-05 21:37:33 +0700

    Add server configuration to allow specific hosts

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['unhating-shemika-ropy.ngrok-free.dev'],
+  },
 })
